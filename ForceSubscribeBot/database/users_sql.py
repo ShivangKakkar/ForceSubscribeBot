bdf3387b36fdc43fdcae3722dfdcf7da6e50cc04
Stark Bots <88478059+StarkBotsIndustries@users.noreply.github.com>
2022-01-02 18:58:49 +0530

    sqlalchemy BIGINT in favour of 64-bit IDs

--- ForceSubscribeBot/database/users_sql.py
+++ ForceSubscribeBot/database/users_sql.py
@@ -1,11 +1,11 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, BIGINT, String
 from . import BASE, SESSION
 
 
 class Users(BASE):
     __tablename__ = "users"
     __table_args__ = {'extend_existing': True}
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(BIGINT, primary_key=True)
     channels = Column(String, nullable=True)
 
     def __init__(self, user_id):
