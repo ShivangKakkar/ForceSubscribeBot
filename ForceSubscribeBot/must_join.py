0a33945beae0d19ecf71aba15c7b7e6ecbdaf412
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2022-05-06 11:24:38 +0530

    Update must_join.py

--- ForceSubscribeBot/must_join.py
+++ ForceSubscribeBot/must_join.py
@@ -22,7 +22,7 @@ async def must_join_channel(bot: Client, msg: Message):
                     f"You must join [this channel]({link}) to use me. After joining try again !",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("✨ Join Channel ✨", url=link)]
+                        [InlineKeyboardButton("Join Our Channel For New Update Or More✅", url=link)]
                     ])
                 )
                 await msg.stop_propagation()
