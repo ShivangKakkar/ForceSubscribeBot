5eaf5c14335bd2762d99c8b25182f1e7359d8c43
mikurobot <113830175+mikurobot@users.noreply.github.com>
2023-09-13 06:46:23 +0700

    Update must_join.py

--- ForceSubscribeBot/must_join.py
+++ ForceSubscribeBot/must_join.py
@@ -19,10 +19,10 @@ async def must_join_channel(bot: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"You must join [this channel]({link}) to use me. After joining try again !",
+                    f"join channel/gc dulu kak [this channel]({link}) to use me. After joining try again !",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("✨ Join Channel ✨", url=link)]
+                        [InlineKeyboardButton(" Join ", url=link)]
                     ])
                 )
                 await msg.stop_propagation()
